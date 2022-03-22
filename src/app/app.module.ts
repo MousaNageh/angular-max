dc6951f7282e90e91afb329024a5763b82f69e40
mousanageh <200moussa200@gmail.com>
2022-03-22 15:47:43 +0200

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AlertComponent } from './shared/alert/alert.component';
+// import { RecipeModule } from './recipes/recipes.module';
+// import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    // RecipeModule,
+    // ShoppingListModule,
+    AuthModule,
+    SharedModule,
+    CoreModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AlertComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import {  ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
+// import { RouterModule } from "@angular/router";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeRoutingModule } from "./recipes-routing.module";
+import { RecipesComponent } from "./recipes.component";
+
+
+@NgModule({
+  declarations:[
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeDetailComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+    RecipeItemComponent
+  ],
+  imports:[
+    // CommonModule,
+    // RouterModule,
+    ReactiveFormsModule,
+    RecipeRoutingModule,
+    SharedModule
+  ],
+  exports:[
+    // RecipesComponent,
+    // RecipeListComponent,
+    // RecipeDetailComponent,
+    // RecipeDetailComponent,
+    // RecipeStartComponent,
+    // RecipeEditComponent,
+    // RecipeItemComponent
+  ]
+})
+export class RecipeModule{}
